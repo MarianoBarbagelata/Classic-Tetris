781b639dfeddf9954e42174b4bc78b2bbcc8290e
MarianoBarbagelata <marianoabarba@gmail.com>
2021-09-24 17:59:26 -0300

    The proyect is uploaded

---
+++ src/main/Loader.java
@@ -0,0 +1,34 @@
+package main;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+
+public class Loader {
+	
+	public static BufferedImage loadImage(String path){
+		try {
+			return ImageIO.read(Loader.class.getResource(path));
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		
+		return null;
+	}
+	
+	public static Clip LoadSound(String direction) {
+		try {
+			Clip clip = AudioSystem.getClip();
+			clip.open(AudioSystem.getAudioInputStream(Loader.class.getResource(direction)));
+			return clip;
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
